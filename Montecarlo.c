f78757a1923690714a3913d52662c65a3f6a7531
tomcraju <84323852+tomcraju@users.noreply.github.com>
2021-06-02 23:53:48 +0530

    Create Montecarlo.c

---
+++ Montecarlo.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+float wallis_pi(int n)
+ {	float pi;
+ 	float prod=1;
+ 	for(int i=1;i<=n;i++){
+ 	float m = (4.0*i*i)/((4.0*i*i)-1);
+ 	prod=prod*m;
+ 	}
+ 	pi=2*prod;
+ 	return pi;
+}
+
+
+
+int main(void) {
+  float pi;
+  for (int i=0; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      abort();
+    }
+  }
+
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
+    }
+  }
+}
